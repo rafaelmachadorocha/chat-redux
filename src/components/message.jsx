a919a7c8feacd2ac5ef1d2e3e3a325e325050924
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-10-09 03:38:34 -0300

    implementing function to display different names in different colors

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 
 class Message extends Component {
   render() {
+    const color = this.intToRGB(this.hashCode(this.props.author))
     return (
       <div className="message">
-        <p className="header"><span className="author">{this.props.author}</span> - <small className="time">{this.formatTime(this.props.createdAt)}</small></p>
+        <p className="header"><span className="author" style={{color: `#${color}`}}>{this.props.author}</span> - <small className="time">{this.formatTime(this.props.createdAt)}</small></p>
         <p className="content">{this.props.content}</p>
       </div>
     )
+  }
+
+  hashCode = (str) => {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      // eslint-disable-next-line no-bitwise
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
+
+  intToRGB = (i) => {
+    let c = (i & 0x00FFFFFF).toString(16).toUpperCase();
+    return "00000".substring(0, 6 - c.length) + c;
   }
 
   formatTime = (time) => {
